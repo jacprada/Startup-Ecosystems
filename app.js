88b4e79849721a22b68301022246390e1fb0166a
jacprada <jacopo.prada@gmail.com>
2015-09-12 13:45:23 +0100

    setting up app.js file

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var express = require("express");
-var app     = express();
+var express     = require("express");
+var app         = express();
+var bodyParser  = require("body-parser");
+var morgan      = require("morgan");
+var mongoose    = require('mongoose');
+
