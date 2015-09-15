8ee8732f3b7f6b2cd981ed3dcbbc6e6b3cb2d952
jacprada <jacopo.prada@gmail.com>
2015-09-15 13:44:11 +0100

    controller added to controller hub

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router  = express.Router();
 
 router.use('/hubs', require('./hub'));
+router.use('/startups', require('./startup'));
 router.use('/auth', require('./authenticationController'));
 router.use('/users', require('./usersController'));
 
