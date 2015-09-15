61c9f8edd16842516e3ba3f7200a813f2e2dfe04
jacprada <jacopo.prada@gmail.com>
2015-09-15 13:38:37 +0100

    crawler added

---
+++ controllers/startup.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router  = express.Router();
+
+var Startup = require('../models/startup');
+
+
+// INDEX 
+router.get('/', function(req, res){
+  Startup.find()
+  .exec(function(error, startups){
+    if(error)return res.status(404).json({message: 'Could not find any startups'})
+      return res.status(200).send(startups);
+  });
+});
+
+
+// SHOW
+router.get('/:id', function(req,res){
+  var id = req.params.id;
+  Startup.findById({_id: id})
+  .exec(function(error, startup){
+    if(error) return res.status(404).send({message: 'Could not find startup'})
+      return res.status(200).send(startup);
+  });
+});
--- models/startup.js
+++ models/startup.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 
 var StartupSchema = new mongoose.Schema({
   id: Number,
+  externalId: Number,
   location: String,
   name: String,
   bio: String,
-  href: String,
+  url: String,
   twitter: String,
   image: String
 });
 
-var Startup = mongoose.model("Startup", StartupSchema);
-module.exports = Startup;
+module.exports = mongoose.model("Startup", StartupSchema);
